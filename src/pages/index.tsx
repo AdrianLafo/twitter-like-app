ed1646ed616f835f0fb6ac087f11781eac6bc6ae
AdrianLafo <adrianlafontaine@gmail.com>
2023-06-10 21:35:36 -0600

    posts model and router created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import { api } from "~/utils/api";
 import { SignInButton, useUser, SignOutButton} from "@clerk/nextjs";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const { isSignedIn } = useUser()
+  const user = useUser()
+  const {data} = api.posts.getAll.useQuery();
+
 
   return (
     <>
@@ -17,9 +18,12 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
-          {!isSignedIn && <SignInButton />}
-          {!!isSignedIn && <SignOutButton />}
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
         </div> 
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
